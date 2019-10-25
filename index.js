fdba7961d76961e4140e79bf18d6aff1a27f911c
Michael Weedman <michael.weedman@gmail.com>
2019-10-25 13:21:28 -0700

    Some Pseudocode

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 'use strict';
+let score = 0;
+let questionNumber = 0;
+
 
 //Render Function
+function renderQuiz(){
+
+}
 
 //Submit Function **has to work with keyboard
+function submitAnswer(){
+  //this function will submit the user input
+}
+//Check answer function
+function checkAnswer(){
+  //this function will check the answer input against the 
+  //correct answer from the STORE array.
+}
 
 //Next Function
 
