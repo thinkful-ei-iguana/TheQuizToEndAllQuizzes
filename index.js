0e32b5182e4fb69aa2c3231edeb3b969e06225b7
Michael Weedman <michael.weedman@gmail.com>
2019-10-25 13:52:33 -0700

    more pseudocoding

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 'use strict';
-let score = 0;
-let questionNumber = 0;
-
 
 //Render Function
 function renderQuiz(){
-
+  //Filled with the html elements of the quiz form.
 }
 
 //Submit Function **has to work with keyboard
@@ -19,7 +16,43 @@ function checkAnswer(){
 }
 
 //Next Function
+function nextQuestion(){
+  //this function will take a button press and move us
+  //to the next question in the STORE.  Will need to 
+  //
+}
+
+//Function to Move Quiz to Results Page
+function resultsPage(){
+  //If question number advances past 5, move to this page
+  //instead of trying to render another question.
+  //This page will contain the restart quiz button.
+
+}
 
 //Function Start Quiz
+function startQuiz(){
+// upon hitting the start quiz button, this will render the first
+// quiz question
+}
 
 //Function Restart Quiz
+function restartQuiz(){
+  //when you click the button, resets the question number and score
+  //and re-renders the quiz.
+}
+
+//Function Update Score
+function updateScore(Score){
+  //this function will add to score if the
+  //person got the previous question correct.
+  //will need to call a comparison function which compares
+  //answer to the correctAnswer in STORE.
+}
+
+
+//Function Update Question Number
+function updateQuestionNumber(){
+  //this function will add to the question number
+  //every time that the next question button is invoked.
+}
