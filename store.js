05663547b4ac3c785edb38d5e0d74dce019a70c2
Michael Weedman <michael.weedman@gmail.com>
2019-10-25 12:58:24 -0700

    Created the Store Object

--- store.js
+++ store.js
@@ -1,2 +1,22 @@
 'use strict';
-const STORE = [{},{},{},{},{},];
+const STORE = {
+  questions:[
+  {question: 'When was the first known quiz given?',
+  answers: ['1780', '1883', '2000', '1560'],
+  correctAnswer: '1780',},
+  { question: 'Which country is credited with the first mention of the word quiz?',
+    answers: ['Ireland', 'Mexico', 'Spain', 'Australia'      ],
+    correctAnswer: 'Ireland',},
+  { question: 'How many people took the largest quiz in History?',
+    answers: ['2280', '12000', '1823', '9183'],
+    correctAnswer: '2280',},
+  { question: 'Which of the following are popular Competition Quizzes?',
+    answers: ['Jeopardy', 'The Weakest Link', 'Quiz bowl', 'World Quizzing Championship', 'All of the above'],
+    correctAnswer: 'All of the above',},
+  { question: 'Countries in which quizzes are forbidden?',
+    answers: ['China', 'Croatia', 'Canada', 'Chile', 'None of the above'],
+    correctAnswer: 'None of the above'}
+  ],
+  score: 0,
+  questionNumber: 0
+};
